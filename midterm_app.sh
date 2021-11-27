0062260ba4f8d9ca806935788fbaf1a795df1982
jtaway-tip <89887826+jtaway-tip@users.noreply.github.com>
2021-11-27 01:37:24 +0000

    Add files via upload

--- midterm_app.sh
+++ midterm_app.sh
@@ -11,7 +11,7 @@ echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  midterm_app.py.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY  midterm_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5000" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/midterm_app.py" >> tempdir/Dockerfile
 
